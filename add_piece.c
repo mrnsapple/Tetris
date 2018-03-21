7dc7b7d32aaf894a856dda5e5112369adf6a06be
Oriol Manzano <oriol.manzano@epitech.eu>
2018-03-21 23:18:38 +0100

    K

--- add_piece.c
+++ add_piece.c
@@ -7,6 +7,31 @@
 
 #include "list.h"
 
+int	move_piece(char **av, char **reference)
+{
+	int	i = 0;
+	int	g = -1;
+	int	x;
+	
+	for (int y = 0; reference[y + 1] != NULL; y++, g = -1) {
+		for (x = 0; reference[y][x] != '\0'; x++) {
+			if (reference[y][x] == '*')
+				i = 1;
+			if (reference[y + 1][x] == '*')
+				g = 0;
+		}
+		if (i == 1 && g == -1) {
+			printw("hehe");
+			printw(reference[y - 1]);
+			printw("hehe");
+			printw(reference[y]);
+			printw("\n");
+			return (0);
+		}
+	}
+	return (0);
+}
+
 void	add_piece(char **av, char **reference,pieces_t *a)
 {
 	int	i = 0;
@@ -23,6 +48,10 @@ void	add_piece(char **av, char **reference,pieces_t *a)
 				reference[y + 1][x + 1] = a->map[y][x];
 			}
 	}
+	else
+		move_piece(av, reference);
+	
+		
 	//print_map(av);
 	
 //its needed to add another piece
