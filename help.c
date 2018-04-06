cbedde73684d4bcb68a3dff15efc835dd03e80b8
Oriol Manzano <oriol.manzano@epitech.eu>
2018-04-06 11:56:58 +0200

    K

--- help.c
+++ help.c
@@ -12,9 +12,11 @@ int	help(int ac, char **av)
 	int	fd;
 	char	buff[2];
 	int	size = 0;
-//	int	i = 0;
 
 	fd = open("help_text" , O_RDONLY, 0644);
+	my_putstr("Usage:  ");
+	my_putstr(av[0]);
+	my_putstr(" [options]\n");
 	if (fd == -1)
 		return (0);
 	for (size = read(fd, buff, 1); size > 0;
